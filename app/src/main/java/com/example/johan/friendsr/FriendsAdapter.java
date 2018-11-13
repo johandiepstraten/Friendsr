684cb59156bf4a8f58ab5d07bb6b841acaff4447
johandiepstraten <johandiepstraten@gmail.com>
2018-11-13 13:31:55 +0100

    Initial Project

---
+++ app/src/main/java/com/example/johan/friendsr/FriendsAdapter.java
@@ -0,0 +1,38 @@
+package com.example.johan.friendsr;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendsAdapter extends ArrayAdapter<Friend> {
+    private ArrayList<Friend> friends;
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
+        }
+        Friend currentFriend = friends.get(position);
+        String friend_name = currentFriend.getName();
+//        Drawable friend_picture = getContext().getDrawable(currentFriend.getDrawableId());
+        ((TextView) convertView.findViewById(R.id.FriendName)).setText(friend_name);
+        ((ImageView) convertView.findViewById(R.id.FriendPicture)).setImageResource(currentFriend.getDrawableId());
+        return convertView;
+    }
+
+
+    public FriendsAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Friend> objects) {
+        super(context, resource, objects);
+        friends = objects;
+    }
+}
--- app/src/main/java/com/example/johan/friendsr/MainActivity.java
+++ app/src/main/java/com/example/johan/friendsr/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.johan.friendsr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Printer;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Friend> friends = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        friends.add(new Friend("Sanne", "Ik ben ook messed up", getResources().getIdentifier("sanne2",
+                "drawable", getPackageName())));
+        friends.add(new Friend("Annemijn", "Wikipediawedstrijd?", getResources().getIdentifier("annemijn",
+                "drawable", getPackageName())));
+        friends.add(new Friend("Man Sam", "Waar is Daantje?", getResources().getIdentifier("mansam",
+                "drawable", getPackageName())));
+        friends.add(new Friend("Jan", "Ik mediteer 10 uur per dag", getResources().getIdentifier("jan",
+                "drawable", getPackageName())));
+        friends.add(new Friend("Mendel", "Ik kan niet", getResources().getIdentifier("mendel",
+                "drawable", getPackageName())));
+        friends.add(new Friend("Rebecca", "De wereld is een regenboog en de pot met goud staat in een steegje naast de Amstel54", getResources().getIdentifier("rebecca",
+                "drawable", getPackageName())));
+        friends.add(new Friend("Steven", "Ik ben beter geworden in opruimen door Edinburgh", getResources().getIdentifier("steven",
+                "drawable", getPackageName())));
+        friends.add(new Friend("Vrouw Sam", "Wereldreiziger met een zachter g", getResources().getIdentifier("vrouwsam",
+                "drawable", getPackageName())));
+        friends.add(new Friend("Ya'gel", "Kom naar Krit", getResources().getIdentifier("yagel",
+                "drawable", getPackageName())));
+
+        FriendsAdapter adapter = new FriendsAdapter(this, R.layout.grid_item, friends);
+        GridView testgridview = findViewById(R.id.gridview);
+
+        testgridview.setAdapter(adapter);
     }
 }
